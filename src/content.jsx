fa427b13040a1a1e7a53ef126239504e63d20036
Shifara <135446502+Shifaratesfaye@users.noreply.github.com>
2024-01-06 15:30:32 +0000

    dsd

---
+++ src/content.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Content = () => {
+  return (
+    <div className="content">
+      <div className="top-right">
+        <img src="user.png" class="content-img" alt="My Photo" />
+      </div>
+      <div className="bottom-left">
+        <h1>ALL Thing Simply!</h1>
+      <p>Mobile App Developement
+       I develop feature-rich and interactive mobile applications for Android and iOS devices, using the most advanced technologies and frameworks. .
+      Deploynment & maintenance
+      I deploy maintain configure and test for user usability, security and effiecency of you website under work.</p>
+         </div>
+      <div className="bottom-right">
+      <p>I design and develop custom websites for your business, 
+            using the best in trend programming language. 
+            I also create responsive, user-friendly, and
+             SEO-optimized websites that attract more visitors and conversions..</p>
+     
+      </div>
+    </div>
+  );
+};
+
+export default Content;
